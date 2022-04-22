3698e01723f1598c301497a77f2ada829fd0b439
tarcisiozf <tar-2008.ferraz@hotmail.com>
2022-04-22 15:44:32 -0300

    draft for controller and service

---
+++ server/src/controllers/TransactionController.ts
@@ -0,0 +1,19 @@
+import {NextFunction, Request, Response} from "express";
+import {TransactionService} from "../services/TransactionService";
+
+export class TransactionController {
+    constructor(private readonly transactionService: TransactionService) {
+    }
+
+    async splitPay(req: Request, res: Response, next: NextFunction) {
+        try {
+            const tx = await this.transactionService.splitPay()
+
+            res.json({
+                transaction: tx.serialize().toString('base64')
+            })
+        } catch (e) {
+            next(e)
+        }
+    }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,17 @@
+import {clusterApiUrl, Connection} from "@solana/web3.js";
+import {TransactionController} from "./controllers/TransactionController";
+import {TransactionService} from "./services/TransactionService";
+
 const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+const transactionService = new TransactionService(connection)
+const transactionController = new TransactionController(transactionService)
+
+app.get('/transaction', transactionController.splitPay)
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`)
