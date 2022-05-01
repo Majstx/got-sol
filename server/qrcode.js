23daed66656cda979dc58f8e76199ace1f170b4b
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-05-01 10:34:51 -0300

    add recipient

--- server/qrcode.js
+++ server/qrcode.js
@@ -6,6 +6,10 @@ const url = new URL(process.env.GOT_SOL_APP_URL);
 url.pathname = "/transaction";
 url.searchParams.set("amount", "2.2");
 url.searchParams.set("label", "tx label");
+url.searchParams.set(
+  "recipient",
+  "AbUV7m5KCcfYWM1sC9TMVZyPnXSBFTHS8QnKLwdJFj3x"
+);
 
 const content = "solana:" + encodeURIComponent(url.toString());
 
