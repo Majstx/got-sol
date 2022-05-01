f8ba85f839b8865c66b813fc16338dc5fabf5aa1
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-04-30 21:16:38 -0300

    logo endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import { Cluster, clusterApiUrl, Connection } from "@solana/web3.js";
+import { join } from "path";
 import { TransactionController } from "./controllers/TransactionController";
 import { TransactionService } from "./services/TransactionService";
 
@@ -13,6 +14,11 @@ const endpoint = clusterApiUrl(process.env.GOT_SOL_SOLANA_CLUSTER as Cluster);
 const connection = new Connection(endpoint, "confirmed");
 const transactionService = new TransactionService(connection);
 const transactionController = new TransactionController(transactionService);
+
+app.get('/logo', (req, res) => {
+  const path = join(__dirname, '../resources/logo.jpeg')
+  res.sendFile(path)
+})
 
 app.get(
   "/transaction",
