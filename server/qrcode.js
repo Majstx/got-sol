1e9e9d954eb1776bacfbdc70d21c7cc3549b476a
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-05-01 14:08:41 -0300

    qrcode to file

--- server/qrcode.js
+++ server/qrcode.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 
 const qr = require("qrcode");
+const path = require("path");
 
 const url = new URL(process.env.GOT_SOL_APP_URL);
 url.pathname = "/transaction";
-url.searchParams.set("amount", "2.2");
-url.searchParams.set("label", "tx label");
+url.searchParams.set("amount", "0.5");
 url.searchParams.set(
   "recipient",
   "AbUV7m5KCcfYWM1sC9TMVZyPnXSBFTHS8QnKLwdJFj3x"
 );
+url.searchParams.set(
+  "splToken",
+  "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+);
 
 const content = "solana:" + encodeURIComponent(url.toString());
+const filePath = path.join(__dirname, "qrcode.png");
 
-qr.toString(content, { type: "terminal" }, (err, data) => {
-  console.clear();
-  console.log(url.toString());
-  console.log(data);
-});
+qr.toFile(filePath, content);
