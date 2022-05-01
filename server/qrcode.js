a74e1025b6fbf7526b7bcb2ae9238e1024b770cf
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-04-30 22:30:38 -0300

    fix qrcode

--- server/qrcode.js
+++ server/qrcode.js
@@ -2,8 +2,15 @@ require("dotenv").config();
 
 const qr = require("qrcode");
 
-const data = "solana:" + encodeURIComponent(process.env.GOT_SOL_APP_URL);
+const url = new URL(process.env.GOT_SOL_APP_URL);
+url.pathname = "/transaction";
+url.searchParams.set("amount", 5);
 
-qr.toString(data, { type: "terminal" }, (err, data) => {
+const content = "solana:" + encodeURIComponent(url.toString());
+
+qr.toString(content, { type: "terminal" }, (err, data) => {
+  console.clear();
+  console.log(url.toString());
+  console.log(content);
   console.log(data);
 });
