2682a56939c456294c04ed137a0eed612116598a
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-05-01 10:04:00 -0300

    add label to qrcode

--- server/qrcode.js
+++ server/qrcode.js
@@ -4,13 +4,13 @@ const qr = require("qrcode");
 
 const url = new URL(process.env.GOT_SOL_APP_URL);
 url.pathname = "/transaction";
-url.searchParams.set("amount", 5);
+url.searchParams.set("amount", "2.2");
+url.searchParams.set("label", "tx label");
 
 const content = "solana:" + encodeURIComponent(url.toString());
 
 qr.toString(content, { type: "terminal" }, (err, data) => {
   console.clear();
   console.log(url.toString());
-  console.log(content);
   console.log(data);
 });
