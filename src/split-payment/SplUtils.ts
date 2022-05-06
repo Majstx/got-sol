250a12eb1ca81cf2657fe351787cfeb7c62f1af4
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-05-06 15:54:08 -0300

    add lru cache

--- src/split-payment/SplUtils.ts
+++ src/split-payment/SplUtils.ts
@@ -7,19 +7,47 @@ import {
   TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
 import { Connection, PublicKey, TransactionInstruction } from "@solana/web3.js";
-import { inject, injectable } from "tsyringe";
+import { inject, injectable, singleton } from "tsyringe";
+import LRUCache from "lru-cache";
 
 @injectable()
+@singleton()
 export class SplUtils {
-  constructor(@inject(Connection) private readonly connection: Connection) {}
+  private readonly ataCache: LRUCache<string, PublicKey>;
+  private readonly accountCache: LRUCache<string, Account>;
 
-  getAssociatedTokenAddress(splToken, owner): Promise<PublicKey> {
-    return getAssociatedTokenAddress(splToken, owner);
+  constructor(@inject(Connection) private readonly connection: Connection) {
+    this.ataCache = new LRUCache({ max: 64 });
+    this.accountCache = new LRUCache({ max: 64 });
+  }
+
+  async getAssociatedTokenAddress(
+    splToken: PublicKey,
+    owner: PublicKey
+  ): Promise<PublicKey> {
+    const key = splToken.toString() + owner.toString();
+    if (this.ataCache.has(key)) {
+      return this.ataCache.get(key);
+    }
+
+    const ata = await getAssociatedTokenAddress(splToken, owner);
+    this.ataCache.set(key, ata);
+
+    return ata;
   }
 
   async getAccount(ata: PublicKey): Promise<Account | null> {
+    const key = ata.toString();
+
+    if (this.accountCache.has(key)) {
+      return this.accountCache.get(key);
+    }
+
     try {
-      return await getAccount(this.connection, ata);
+      const acc = await getAccount(this.connection, ata);
+      this.accountCache.set(key, acc);
+
+      return acc;
     } catch (err) {
       return null;
     }
