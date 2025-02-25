6b77077a876e670fa402a99ebbeb2d5c4893aa4b
Remedy <Majestic.Advising@gmail.com>
2025-02-24 21:22:50 -0400

    Fix _app.tsx with complete wallet adapter setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
 import type { AppProps } from 'next/app';
 import { useMemo } from 'react';
 import dynamic from 'next/dynamic';
